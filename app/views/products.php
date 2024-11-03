f1767d1b5eceed87118cdeeae898b2b66128b94f
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-11-03 23:38:08 +0700

    Enhance product filtering with AJAX and update layout for search results

--- app/views/products.php
+++ app/views/products.php
@@ -1,9 +1,10 @@
 <?php
 require_once __DIR__ . '/layout/MainLayout.php';
 
+
 $data = [
     'pageTitle' => 'Products',
-    'infoUser' => false,
 ];
+
 
 renderMainLayout(__DIR__ . '/components/products_content.php', $data);
