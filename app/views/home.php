ff8f67acf02fe5b86b148e676cf98e2e549e1433
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-11-03 23:39:31 +0700

    Refactor home page layout by removing unnecessary HTML elements and improving structure

--- app/views/home.php
+++ app/views/home.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/layout/MainLayout.php';
 
 $data = [
     'pageTitle' => 'Home',
-    'infoUser' => false,
 ];
 
 renderMainLayout(__DIR__ . '/components/home_content.php', $data);
